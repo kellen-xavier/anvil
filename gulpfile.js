cca4af4d27163e4facc2a48fcd8ac315014007ae
João Pedro Gonçalves <joao.kamun@gmail.com>
2017-03-30 16:44:33 -0300

    Added p in input-group style

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./docs/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
 	gulp.watch('./scss/**/*.scss', ['sass']);
 });
 
